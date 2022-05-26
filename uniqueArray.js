caf000175a5903da5f4ec968247248cc63c5f5dc
rajarshi-cn <rajarshi@capitalnumbers.com>
2022-05-26 10:49:30 +0530

    first push

--- uniqueArray.js
+++ uniqueArray.js
@@ -2,28 +2,32 @@
 const uniqueAges = ages.filter((value,index,self)=> self.indexOf(value)==index)
 console.log(uniqueAges);*/
 
-const ages1 = [26, 27, 26, 26, 28, 28, 29, 29, 30]
-const uniqueAges1 = [...new Set(ages1)]     //In ES6 Set method is used to printout unique object elements
+const ages1 = [26, 27, 26, 26, 28, 28, 29, 29, 30];
+const uniqueAges1 = [...new Set(ages1)]; //In ES6 Set method is used to printout unique object elements
 
-console.log(uniqueAges1)
+console.log(uniqueAges1);
 
 const students = [
-    {
-      name: 'Krunal',
-      age: 26
-    }, 
-    {
-      name: 'Ankit Singh',
-      age: 25
-    },
-    {
-      name: 'Krunal',
-      age: 26
-    },
-    {
-      name: 'Anurag',
-      age: 25
-    }
-  ]
-  const uniqueArr = [... new Set(students.map(data => data.name))]
-  console.log(uniqueArr)
+  {
+    name: "Krunal",
+    age: 26,
+  },
+  {
+    name: "Ankit Singh",
+    age: 25,
+  },
+  {
+    name: "Krunal",
+    age: 26,
+  },
+  {
+    name: "Anurag",
+    age: 25,
+  },
+  {
+    name: "Kalu",
+    age: 250,
+  },
+];
+const uniqueArr = [...new Set(students.map((data) => data.name))];
+console.log(uniqueArr);
